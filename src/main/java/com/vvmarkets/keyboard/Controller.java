0da519b479fa902498cc4d79c9811a7ef1513d00
latsha <latipovsharif@gmail.com>
2019-09-03 18:19:48 +0500

    fix

--- src/main/java/com/vvmarkets/keyboard/Controller.java
+++ src/main/java/com/vvmarkets/keyboard/Controller.java
@@ -1,4 +1,17 @@
 package com.vvmarkets.keyboard;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class Controller {
+    @FXML
+    Button btnEnter;
+
+    @FXML
+    void initialize() {
+        btnEnter.addActionListener(e -> {
+            System.out.println("btn clicked");
+        });
+    }
 }
