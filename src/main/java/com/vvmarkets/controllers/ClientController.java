d4e490984d0f50836797a96d2a08ac695a803cb8
latsha <latipovsharif@gmail.com>
2019-10-20 16:55:34 +0500

    fix

---
+++ src/main/java/com/vvmarkets/controllers/ClientController.java
@@ -0,0 +1,17 @@
+package com.vvmarkets.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ClientController {
+    @FXML
+    public ComboBox cmbSearchType;
+    @FXML
+    public TextField txtSearch;
+    @FXML
+    public Button btnSearch;
+
+
+}
