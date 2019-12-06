7faa392964feaf0b21f99d3b95db371590732cec
Sharif <latipovsharif@gmail.com>
2019-12-06 21:59:08 +0500

    added config for cash

---
+++ src/main/java/com/vvmarkets/configs/RemoteConfig.java
@@ -0,0 +1,66 @@
+package com.vvmarkets.configs;
+
+import com.vvmarkets.core.Utils;
+import com.vvmarkets.utils.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class RemoteConfig {
+    public enum ConfigType {
+        GENERAL("general"),
+        PRINTER("printer");
+
+        private String type;
+
+        ConfigType(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return this.type;
+        }
+    }
+
+    public enum ConfigSubType {
+        STORE_NAME("storeName"),
+        PRINTER_NAME("receiptPrinter");
+
+        private String type;
+
+        ConfigSubType(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return this.type;
+        }
+    }
+
+    private static String getConfig(ConfigType configType, ConfigSubType subType) {
+        String res = null;
+
+        try (Connection c = db.getConnection()) {
+            ResultSet rs;
+            try (PreparedStatement ps = c.prepareStatement(
+                    "select cco.value " +
+                            "from cash_config_options cco " +
+                            "join cash_configs cc on cco.cash_config_id = cc.id " +
+                            "where cc.name = ? and cco.name = ? limit 1"
+            )) {
+                ps.setString(1, configType.getType());
+                ps.setString(2, subType.getType());
+                rs = ps.executeQuery();
+            }
+
+            if (rs.next()) {
+                res = rs.getString(1);
+            }
+        } catch (Exception e) {
+            Utils.logException(e, "cannot get config for cash");
+        }
+
+        return res;
+    }
+}
