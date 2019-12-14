7b8c93dc1f41a3e1e6642fb6b5e6ca1a70eeca11
Sharif <latipovsharif@gmail.com>
2019-12-15 02:31:58 +0500

    changed main window look

--- src/main/java/com/vvmarkets/core/TabUtil.java
+++ src/main/java/com/vvmarkets/core/TabUtil.java
@@ -2,7 +2,6 @@ package com.vvmarkets.core;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 import java.time.LocalTime;
@@ -16,7 +15,6 @@ public class TabUtil {
 
         Tab newTab = new Tab();
         Label lbl = new Label(time.format(formatter));
-        lbl.setStyle("-fx-rotate: 0; -fx-wrap-text: true;");
         lbl.setWrapText(true);
         lbl.setTextAlignment(TextAlignment.JUSTIFY);
 
